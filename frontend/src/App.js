f674260e8a39b5480f8a174eb4fc444eb7e75032
Jacqueline Schmidt <jschmidt108@gmail.com>
2022-05-26 15:42:15 -0400

    frontend functionality working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,106 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 
-  const [name, setName] = useState('')
-  const [release, setRelease] = useState('')
-  const [image, setImage] = useState('')
-  const [genre, setGenre] = useState('')
+  const [name, setName] = useState()
+  const [release, setRelease] = useState()
+  const [image, setImage] = useState()
+  const [genre, setGenre] = useState()
   const [watched, setWatched] = useState(false)
-  const [active, setActive] = useState('view')
+  const [active, setActive] = useState('index')
   const [movies, setMovies] = useState([])
+
+  const newmovie = (event) => {
+    setActive('create')
+  }
+  const getHome = (event) => {
+    setActive('index')
+  }
+
+  const editMovie = (event) => {
+    setActive('edit')
+  }
+
+  const handleNewNameChange = (event) => {
+    // console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  const handleNewReleaseChange = (event) => {
+    // console.log(event.target.value);
+    setRelease(event.target.value);
+  }
+
+  const handleNewImageChange = (event) => {
+    // console.log(event.target.value);
+    setImage(event.target.value);
+  }
+
+  const handleNewGenreChange = (event) => {
+    // console.log(event.target.value);
+    setGenre(event.target.value);
+  }
+
+  const handleNewWatchedChange = (event) => {
+    // console.log(event.target.checked);
+    setGenre(event.target.checked);
+  }
+
+  const handleNewmovieFormSubmit = (event) => {
+    event.preventDefault();
+    // console.log(newName);
+    axios.post(
+      'http://localhost:3000/movies', 
+      {
+        name: name,
+        release: release,
+        image: image,
+        genre: genre,
+        watched: watched
+      }
+    ).then(() => {
+      axios
+        .get('http://localhost:3000/movies')
+        .then((response) => {
+          setMovies(response.data)
+          setActive('index')
+        })
+    })
+  }
+
+  const handleDelete = (movieData)=>{
+    axios
+        .delete(`http://localhost:3000/movies/${movieData._id}`)
+        .then(()=>{
+            axios
+                .get('http://localhost:3000/movies')
+                .then((response)=>{
+                    setMovies(response.data)
+                })
+        })
+  }
+
+
+  const handleUpdateFormSubmit = (movieData)=>{
+    axios
+        .put(
+            `http://localhost:3000/movies/${movieData._id}`,
+            {
+              name: name,
+              release: release,
+              image: image,
+              genre: genre,
+              watched: watched
+            }
+        )
+        .then(()=>{
+            axios
+                .get('http://localhost:3000/movies')
+                .then((response)=>{
+                    setMovies(response.data)
+                })
+        })
+  }
+
 
   useEffect(() => {
     axios.get('http://localhost:3000/movies').then((response) => {
@@ -20,11 +113,13 @@ const App = () => {
     })
   }, [])
 
-  if (active === 'view') {
+  if (active === 'index') {
     return (
       <>
         <header>
-          <h1> TEST 1 </h1>
+          <h1> Index </h1>
+          <button onClick={getHome}>Home</button>
+          <button onClick={newmovie}>Add a movie!</button>
         </header>
         <div className='container'>
           <div className="row row-cols-1 row-cols-md-3 g-4">
@@ -38,10 +133,8 @@ const App = () => {
                  <h3 className="card-body">{movies.genre}</h3>
                  {movies.watched ? <h3 className="card-body">You have watched this movie</h3> : <h3 className="card-body">You have not watched this yet</h3>}
                </div>
-              <button>Edit movies</button>
-              <button>
-                Delete
-              </button>
+              <button onClick={editMovie}>Edit Movie</button>
+              <button onClick={(event) => {handleDelete(movies)}}>Remove Movie</button>
             </div>
             })
           }
@@ -53,10 +146,24 @@ const App = () => {
     return (
       <>
         <header>
-          <h1> TEST 2 </h1>
+          <h1> Edit movies </h1>
+          <button onClick={getHome}>Home</button>
         </header>
           <div className='container'>
-            <p></p>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="card" key={movies._id}>
+              <div className="card-body">
+                <form onSubmit={(event) => { handleUpdateFormSubmit(movies) }}>
+                  <h5 className="card-title"> Name: <input type="text" className="form-control" onChange={handleNewNameChange} placeholder={movies.name} /></h5> <br />
+                  <p className="card-body">Image: <input type="text" className="form-control" onChange={handleNewImageChange} placeholder={movies.image} /></p> <br />
+                  <p className="card-body">Release: <input type="text" className="form-control" onChange={handleNewReleaseChange} placeholder={movies.release} /> </p> <br />
+                  <p className="card-body">Genre: <input type="text" className="form-control" onChange={handleNewGenreChange} placeholder={movies.genre} /> </p> <br />
+                  Watched: <input type="checkbox" onChange={handleNewWatchedChange} /><br />
+                  <input type="submit" value="SUBMIT" />
+                </form>
+              </div>
+            </div>
+          </div>
           </div>
       </>
     )
@@ -64,11 +171,19 @@ const App = () => {
     return (
       <>
         <header>
-          <h1> TEST 3 </h1>
+          <h1> Create movies! </h1>
+          <button onClick={getHome}>Home</button>
         </header>
-          <div className='container'>
-            <p></p>
-          </div>
+        <div className='container'>
+          <form onSubmit={handleNewmovieFormSubmit}>
+            Name: <input type="text" className="form-control" onChange={handleNewNameChange} /> <br />
+            Image: <input type="text" className="form-control" onChange={handleNewImageChange} /> <br />
+            Release: <input type="text" className="form-control" onChange={handleNewReleaseChange} /> <br />
+            Genre: <input type="text" className="form-control" onChange={handleNewGenreChange} /><br />
+            Watched: <input type="checkbox" onChange={handleNewWatchedChange} /><br />
+            <input type="submit" value="SUBMIT" />
+          </form>
+        </div>
       </>
     )
   }
